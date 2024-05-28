ebbe6af543db40370ec12be6c0106faa55957dd1
stavguo <dmellogu@umich.edu>
2024-05-27 20:42:07 -0400

    refactored duplicate code within phase system

--- src/systems/phaseSystem.ts
+++ src/systems/phaseSystem.ts
@@ -1,5 +1,6 @@
 import {
     Not,
+    Query,
     addComponent,
     defineQuery,
     defineSystem,
@@ -13,44 +14,6 @@ import Player from '../components/player'
 import Enemy from '../components/enemy'
 import GameWorld from '../helpers/gameWorld'
 
-const makePhaseMessage = (text: string, color: string, scene: Phaser.Scene) => {
-    const textObj = scene.add.text(
-        scene.cameras.main.centerX,
-        scene.cameras.main.centerY / 4,
-        text,
-        {
-            color: color,
-        },
-    )
-        .setDepth(100)
-        .setScrollFactor(0, 0)
-        .setOrigin(0.5, 0.5)
-        .setScale(2)
-        .setStroke('#ffffff', 6)
-        .setAlpha(0)
-        .disableInteractive()
-    scene.tweens.chain({
-        targets: textObj,
-        tweens: [
-            {
-                alpha: { from: 0, to: 1 },
-                delay: 1000,
-                duration: 1000,
-                ease: 'Linear',
-            },
-            {
-                alpha: { from: 1, to: 0 },
-                delay: 3000,
-                duration: 1000,
-                ease: 'Linear',
-                onComplete: function () {
-                    textObj.destroy()
-                },
-            },
-        ],
-    })
-}
-
 export default function createPhaseSystem(scene: Phaser.Scene, world: GameWorld, unitSprites: Map<number, Unit>) {
     const movedPlayerQuery = defineQuery([UnitComponent, Player, Moved])
     const notMovedPlayerQuery = defineQuery([UnitComponent, Player, Not(Moved)])
@@ -58,6 +21,50 @@ export default function createPhaseSystem(scene: Phaser.Scene, world: GameWorld,
     const movedEnemyQuery = defineQuery([UnitComponent, Enemy, Moved])
     const notMovedEnemyQuery = defineQuery([UnitComponent, Enemy, Not(Moved)])
     const movedEnemyEnterQuery = enterQuery(movedEnemyQuery)
+    const makePhaseMessage = (text: string, color: string, clearQuery: Query) => {
+        const textObj = scene.add.text(
+            scene.cameras.main.centerX,
+            scene.cameras.main.centerY / 4,
+            text,
+            {
+                color: color,
+            },
+        )
+            .setDepth(100)
+            .setScrollFactor(0, 0)
+            .setOrigin(0.5, 0.5)
+            .setScale(2)
+            .setStroke('#ffffff', 6)
+            .setAlpha(0)
+            .disableInteractive()
+
+        scene.tweens.chain({
+            targets: textObj,
+            tweens: [
+                {
+                    alpha: { from: 0, to: 1 },
+                    delay: 1000,
+                    duration: 1000,
+                    ease: 'Linear',
+                    onStart: function () {
+                        clearQuery(world).forEach((eid) => {
+                            removeComponent(world, Moved, eid)
+                            unitSprites.get(eid).clearTint()
+                        })
+                    },
+                },
+                {
+                    alpha: { from: 1, to: 0 },
+                    delay: 3000,
+                    duration: 1000,
+                    ease: 'Linear',
+                    onComplete: function () {
+                        textObj.destroy()
+                    },
+                },
+            ],
+        })
+    }
     // Initialize map with all enemies as moved
     notMovedEnemyQuery(world).forEach(eid => addComponent(world, Moved, eid))
     return defineSystem((world) => {
@@ -65,12 +72,7 @@ export default function createPhaseSystem(scene: Phaser.Scene, world: GameWorld,
             const greyColor = 0x808080
             unitSprites.get(eid).setTint(greyColor)
             if (notMovedPlayerQuery(world).length === 0) {
-                movedEnemyQuery(world).forEach((eid) => {
-                    removeComponent(world, Moved, eid)
-                    // TODO (1/2): No point tinting enemies if they move on their own with A* later.
-                    unitSprites.get(eid).clearTint()
-                })
-                makePhaseMessage('Enemy Phase', '#ff0000', scene)
+                makePhaseMessage('Enemy Phase', '#ff0000', movedEnemyQuery)
             }
         })
         movedEnemyEnterQuery(world).forEach((eid) => {
@@ -78,11 +80,7 @@ export default function createPhaseSystem(scene: Phaser.Scene, world: GameWorld,
             const greyColor = 0x808080
             unitSprites.get(eid).setTint(greyColor)
             if (notMovedEnemyQuery(world).length === 0) {
-                movedPlayerQuery(world).forEach((eid) => {
-                    removeComponent(world, Moved, eid)
-                    unitSprites.get(eid).clearTint()
-                })
-                makePhaseMessage('Player Phase', '#0000ff', scene)
+                makePhaseMessage('Player Phase', '#0000ff', movedPlayerQuery)
             }
         })
         return world
